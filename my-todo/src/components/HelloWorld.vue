5bc5a0bf298b24a6da53f0a8db891c8297139e96
abidhossain <mohammad.abid.dev@gmail.com>
2024-04-18 16:46:59 +0600

    setup tailwind css

--- my-todo/src/components/HelloWorld.vue
+++ my-todo/src/components/HelloWorld.vue
@@ -14,15 +14,38 @@ const fullName = computed(() => {
 
 const setFirstName = computed({
   get: () => firstName.value.toUpperCase(),
-  set: value => { firstName.value = value.toUpperCase() }
+  set: value => {
+    firstName.value = value.toUpperCase()
+  }
+})
+
+const products = ref([
+  {name: 'Mango', category: 'fruit'},
+  {name: 'Banana', category: 'fruit'},
+  {name: 'Earbuds', category: 'gadgets'},
+  {name: 'Keyboard', category: 'gadgets'},
+])
+
+const selectedCategory = ref('fruit')
+
+const filteredProducts = computed(() => {
+    return products.value.filter((product) => product.category === selectedCategory.value)
 })
 
 </script>
 
 <template>
-  <input type="text" v-model="setFirstName">
-  <input type="text" v-model="lastName">
 
-  {{ setFirstName }} -> {{ firstName}}
+  <ul>
+    <li v-for="product in filteredProducts">{{ product.name }} / {{ product.category }}</li>
+  </ul>
+  <!--  <input type="text" v-model="setFirstName">-->
+  <!--  <input type="text" v-model="lastName">-->
+
+  <!--  {{ setFirstName }} -> {{ firstName}}-->
+
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
 
 </template>
